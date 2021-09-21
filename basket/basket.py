d351c7b7116985e7bbd0307f505bf029c18fbf73
Omarson Omarson <Omarson@MacBook-Pro-Omarson.local>
2021-09-21 14:38:51 +0200

    next commit

--- basket/basket.py
+++ basket/basket.py
@@ -14,3 +14,11 @@ class Basket():
         if 'skey' not in request.session:
             basket = self.session['skey'] = {}
         self.basket = basket
+
+    def add(self, product, qty):
+        product_id = product.id
+
+        if product_id not in self.basket:
+            self.basket[product_id] = {'price': str(product.price), 'qty':int(qty)}
+
+        self.session.modified = True
--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,20 @@
-from django.shortcuts import render
+from django.http.response import JsonResponse
+from store.models import Product
+from django.shortcuts import get_object_or_404, render
+from .basket import Basket
 
 def basket_summary(request):
+    basket = Basket(request)
     return render(request, 'store/basket/summary.html'),
+
+def basket_add(request):
+    basket = Basket(request)
+    if request.Post.get('action') =='post':
+        product_id = int(request.POST.get('productid'))
+        product_qty = int(request.POST.get('productqty'))
+        product = get_object_or_404(Product, id=product_id)
+        basket.add(product=product, qty=product_qty)
+        
+        basketqty = basket.__len__()
+        response = JsonResponse({'qty': basketqty})
+        return response

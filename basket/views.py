28165262d45383924058196e2223249f8dd1c486
omar <beata4072@gmail.com>
2021-09-21 18:55:26 +0200

    next commit

--- basket/views.py
+++ basket/views.py
@@ -5,16 +5,17 @@ from .basket import Basket
 
 def basket_summary(request):
     basket = Basket(request)
-    return render(request, 'store/basket/summary.html'),
+    return render(request, 'store/basket/summary.html', {'basket': basket}),
 
 def basket_add(request):
     basket = Basket(request)
-    if request.Post.get('action') =='post':
+    if request.POST.get('action') == 'post':
         product_id = int(request.POST.get('productid'))
         product_qty = int(request.POST.get('productqty'))
         product = get_object_or_404(Product, id=product_id)
         basket.add(product=product, qty=product_qty)
-        
+
         basketqty = basket.__len__()
         response = JsonResponse({'qty': basketqty})
+        
         return response

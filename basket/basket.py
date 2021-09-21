96b5ad1206d3b89488823ff79f1db12b6da9986e
omar <beata4072@gmail.com>
2021-09-21 19:14:08 +0200

    next commit

--- basket/basket.py
+++ basket/basket.py
@@ -1,5 +1,4 @@
 from decimal import Decimal
-
 from store.models import Product
 
 
--- basket/views.py
+++ basket/views.py
@@ -1,11 +1,15 @@
-from django.http.response import JsonResponse
-from store.models import Product
+from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, render
+
+from store.models import Product
+
 from .basket import Basket
+
 
 def basket_summary(request):
     basket = Basket(request)
-    return render(request, 'store/basket/summary.html', {'basket': basket}),
+    return render(request, 'store/basket/summary.html', {'basket': basket})
+
 
 def basket_add(request):
     basket = Basket(request)
@@ -17,5 +21,29 @@ def basket_add(request):
 
         basketqty = basket.__len__()
         response = JsonResponse({'qty': basketqty})
-        
+        return response
+
+
+def basket_delete(request):
+    basket = Basket(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('productid'))
+        basket.delete(product=product_id)
+
+        basketqty = basket.__len__()
+        baskettotal = basket.get_total_price()
+        response = JsonResponse({'qty': basketqty, 'subtotal': baskettotal})
+        return response
+
+
+def basket_update(request):
+    basket = Basket(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('productid'))
+        product_qty = int(request.POST.get('productqty'))
+        basket.update(product=product_id, qty=product_qty)
+
+        basketqty = basket.__len__()
+        baskettotal = basket.get_total_price()
+        response = JsonResponse({'qty': basketqty, 'subtotal': baskettotal})
         return response

15892e7e3ec24cce339fd44637e9e062b18a0418
omar <beata4072@gmail.com>
2021-09-30 21:03:50 +0200

    cds

--- basket/basket.py
+++ basket/basket.py
@@ -64,6 +64,18 @@ class Basket():
     def get_total_price(self):
         return sum(Decimal(item['price']) * item['qty'] for item in self.basket.values())
 
+    def get_total_price(self):
+
+        subtotal = sum(Decimal(item['price']) * item['qty'] for item in self.basket.values())
+
+        if subtotal == 0:
+            shipping = Decimal(0.00)
+        else:
+            shipping = Decimal(11.50)
+
+        total = subtotal + Decimal(shipping)
+        return total
+
     def delete(self, product):
         """
         Delete item from session data

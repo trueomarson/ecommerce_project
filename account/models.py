ee736aa8042ed3d82c013330ff0e9f43d869400f
omar <beata4072@gmail.com>
2021-10-13 13:45:29 +0200

    commit

--- account/models.py
+++ account/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager,
 from django.core.mail import send_mail
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-from django_countries.fields import CountryField
 
 
 class CustomAccountManager(BaseUserManager):
@@ -44,7 +43,6 @@ class UserBase(AbstractBaseUser, PermissionsMixin):
     about = models.TextField(_(
         'about'), max_length=500, blank=True)
     # Delivery details
-    country = CountryField()
     phone_number = models.CharField(max_length=15, blank=True)
     postcode = models.CharField(max_length=12, blank=True)
     address_line_1 = models.CharField(max_length=150, blank=True)

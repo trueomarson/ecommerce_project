e962d5a5ddc46b2af56ce21f39a9ca6bec625b23
omar <beata4072@gmail.com>
2021-10-19 10:34:57 +0200

    commit

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
+import debug_toolbar
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,6 +12,7 @@ urlpatterns = [
     path('account/', include('account.urls', namespace='account')),
     path('payment/', include('payment.urls', namespace='payment')),
     path('orders/', include('orders.urls', namespace='orders')),
+    path("__debug__/", include(debug_toolbar.urls)),
 ]
 
 if settings.DEBUG:

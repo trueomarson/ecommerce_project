f951b99a7676beb28d1d303b3d79534b25e4a897
omar <beata4072@gmail.com>
2021-10-20 13:38:31 +0200

    commit

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,16 @@
 from django import forms
 from django.contrib import admin
-from mttp.admin import MPTTModelAdmin
+from mptt.admin import MPTTModelAdmin
+
+from .models import (
+    Category,
+    Product,
+    ProductImage,
+    ProductSpecification,
+    ProductSpecyficationValue,
+    ProductType,
+)
+
+admin.site.register(Category, MPTTModelAdmin)
+
+

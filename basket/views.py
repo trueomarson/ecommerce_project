1ba89c08d41c832b4b8b706e5f8b94e97bd20643
omar <beata4072@gmail.com>
2021-10-27 10:25:16 +0200

    commit

--- basket/views.py
+++ basket/views.py
@@ -1,8 +1,6 @@
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, render
-
 from store.models import Product
-
 from .basket import Basket
 
 
--- store/views.py
+++ store/views.py
@@ -9,7 +9,7 @@ def product_all(request):
 
 def category_list(request, category_slug=None):
     category = get_object_or_404(Category, slug=category_slug)
-    products = Product.products.filter(category=category)
+    products = Product.objects.filter(category=category)
     return render(request, 'store/category.html', {'category': category, 'products': products})
 
 

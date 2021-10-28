927456c29ebc3f2fd420ccbf1955b07bdf6e3b26
omar <beata4072@gmail.com>
2021-10-28 08:47:15 +0200

    commit

--- basket/context_processors.py
+++ basket/context_processors.py
@@ -1,5 +1,5 @@
-from store.models import Category
 from .basket import Basket
 
+
 def basket(request):
-    return{'categories': Category.objects.filter(level=0)}
+    return {'basket': Basket(request)}
